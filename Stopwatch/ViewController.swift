b040af5717f0c4afdd1fe2acba006a7a05a18835
Stefan Huynh <huynh.stefan@gmail.com>
2015-03-28 13:14:39 -0400

    Add label, button, and some logic

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
+    @IBAction func startButtonTapped(sender: UIButton) {
+        startButton.setTitle("Stop", forState: UIControlState.Normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Private
+    
+    private func startStopWatch() -> Void {
+        
+    }
+    
+    private func stopStopWatch() -> Void {
+        
+    }
 }
 
